c9c627a9e07730fa4fce72e1001d5e6f696798aa
mukesh-kk <mukeshkushwaha918@gmail.com>
2025-04-08 18:15:06 +0530

    added sington pattern in ts

--- LLD/DesignPatterns/creational/Singleton.ts
+++ LLD/DesignPatterns/creational/Singleton.ts
@@ -1,1 +1,11 @@
-console.log('ok')
+class Singleton{
+    private static instance: Singleton;
+    constructor(){
+        if (Singleton.instance) {
+            return Singleton.instance;
+        }
+        return  Singleton.instance=this
+    }
+}
+
+console.log(new Singleton()==new Singleton())// true
