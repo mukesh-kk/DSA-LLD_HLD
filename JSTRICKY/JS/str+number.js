cbdd78ae2c422cfcaa2c5a272a338771828c8da7
mukesh-kk <mukeshkushwaha918@gmail.com>
2025-06-17 15:19:15 +0530

    made changes for tricky questions

---
+++ JSTRICKY/JS/str+number.js
@@ -0,0 +1,35 @@
+/**
+ * Topic String Catenation ans Coersion
+ * Predict Output :
+ */
+
+console.log('2' - 2 )  
+console.log('2' + 2 )
+console.log( 2+ '2' )
+console.log('2' * 2  )  
+console.log('2' / 2   )
+console.log('1'-10+'1')
+console.log('10' - - '5');  // 15
+console.log(+ []) // 0
+console.log(null + 1);  // ? 1
+console.log(undefined + 1);  // ? NAN
+console.log([] + 1);  // ?'1'
+console.log([] + []);  // ? ''
+console.log([] + {});  // ?  // '[object Object]'
+console.log({} + []);  // ? 0
+console.log(({}) + []);  // '[object Object]'
+console.log(NaN == NaN);  // ? false
+console.log('b' + 'a' + + 'a' + 'a'); //banana
+console.log(!![] + !![]);  //2
+console.log(typeof NaN);  // 'number'
+console.log([1,2] + [3,4]);  // '1,23,4'
+
+
+
+
+
+
+
+/**
+ * 
+ */
---
+++ JSTRICKY/JS/this.js
@@ -0,0 +1,147 @@
+/**
+ * Predict Output
+ * | **Context**               | **`this` refers to**                        |
+| ------------------------- | ------------------------------------------- |
+| Global                    | `window` (non-strict), `undefined` (strict) |
+| Regular function          | `window` or `undefined`                     |
+| Object method             | The object itself                           |
+| Arrow function            | Lexical `this` (outer scope)                |
+| Constructor (`new`)       | Newly created object                        |
+| `call` / `apply` / `bind` | Explicitly defined `this`                   |
+| DOM Event handler         | The HTML element                            |
+| Class method              | The class instance                          |
+
+ */
+
+'use strict' 
+function show() {
+  console.log(this);
+}
+show();
+
+// 2----------->
+
+const obj = {
+  name: "Mukesh",
+  say () {
+    console.log(this.name);
+  },
+  say1:() =>{
+    console.log(this.name);
+  },
+   say2:function() {
+     console.log(this.name);
+  }
+};
+
+
+
+
+// 3-----------------
+
+const person = {
+  name: "Mukesh",
+  greet() {
+    const inner = () => {
+      console.log(this.name);
+    };
+    inner();
+  }
+};
+person.greet();
+
+
+// 4.
+const obj1 = {
+  name: "ToughCrew",
+  sayHi() {
+    setTimeout(function () {
+      console.log(this.name);
+    }, 100);
+  }
+};
+obj1.sayHi();
+// 5
+
+const user = {
+  name: 'Mukesh',
+  greet() {
+    return `Hello, ${this.name}`;
+  }
+};
+
+const { greet } = user;
+console.log(greet());
+
+//6 
+class Crew {
+  constructor(name) {
+    this.name = name;
+  }
+
+  hello() {
+    setTimeout(function () {
+      console.log(this.name);
+    }, 100);
+  }
+}
+
+new Crew("Mukesh").hello();
+
+// Advanced--->
+
+// 6
+
+const obj3 = {
+  name: "Mukesh",
+  outer() {
+    function inner() {
+      console.log(this.name);
+    }
+    inner();
+  }
+};
+obj3.outer();
+//  how to fix
+
+// 7
+const obj4 = {
+  name: "Mukesh",
+  outer() {
+    function mid() {
+      function inner() {
+        console.log(this.name);
+      }
+      inner();
+    }
+    mid();
+  }
+};
+obj4.outer();
+// 8
+const obj5 = {
+  name: "Mukesh",
+  outer: () => {
+    const mid = () => {
+      const inner = () => {
+        console.log(this.name);
+      };
+      inner();
+    };
+    mid();
+  }
+};
+obj5.outer();
+
+// 9
+const obj6 = {
+  multiplier: 3,
+  numbers: [1, 2, 3],
+  multiply() {
+    return this.numbers.map(function (n) {
+      return n * this.multiplier;
+    });
+  }
+};
+
+console.log(obj6.multiply());
