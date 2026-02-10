234ff7c431536d63d06fb0092a349feff406c2df
mukesh-kk <mukeshkushwaha918@gmail.com>
2026-02-10 14:38:46 +0530

    fixed

--- JSTRICKY/JS/nodejs/multiprocess.js
+++ JSTRICKY/JS/nodejs/multiprocess.js
@@ -6,4 +6,24 @@ if (cluster.isMaster)
         for (let i = 0; i < cpus; i++) cluster.fork(); } 
         else { 
             http.createServer((req, res) => { res.end("Hello from worker " + process.pid); }).listen(3000); } 
-            // This lets Node.js use all cores.
+// This lets Node.js use all cores.
+
+
+
+//Process-Level vs Machine-Level Scaling
+
+// Clustering (Single Machine):
+
+// Multiple processes on one server machine
+
+// Master distributes load to workers
+
+// Best for using all CPU cores on one host
+
+// Distributed Load Balancing((nginx)) (Many Machines):
+
+// Each machine runs its own set of Node.js processes
+
+// External load balancer sits in front and distributes traffic across machines
+
+// Useful when traffic outgrows a single machine          
